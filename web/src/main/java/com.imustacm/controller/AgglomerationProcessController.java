4bb48eaf05e8f061f43058ced7b2bce4a1fd2611
SuperCat <1944462162@qq.com>
2019-10-16 11:17:20 +0800

    完成了swagger的配置

--- web/src/main/java/com.imustacm/controller/AgglomerationProcessController.java
+++ web/src/main/java/com.imustacm/controller/AgglomerationProcessController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashMap;
 import java.util.List;
